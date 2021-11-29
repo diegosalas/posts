43713e2281f4ce4e5a464a8a1835983066eac95d
Diego Perez <diegosalas@gmail.com>
2021-11-29 13:34:58 +0100

    revert

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('miapp2 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('app is running!');
   });
 });
